ea3d6cc4ddaed9d3c363d491521841877f770253
amritmathur11 <71231336+amritmathur11@users.noreply.github.com>
2020-12-01 17:02:06 -0500

    Add files via upload

---
+++ SimpleWebAPI/Controllers/RandomController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        [HttpGet]
+        public int Get()
+
+        {
+            Random random = new Random();
+            double flt = random.NextDouble();
+            int shift = Convert.ToInt32(Math.Floor(25 * flt));
+            return shift;
+        }
+        [HttpPost]
+        public double Post()
+
+        {
+
+            Random random = new Random();
+
+            double flt = random.NextDouble();
+            return flt;
+        }
+        [HttpPut]
+        public string Put()
+
+        {
+
+            return "Thank you for the new number!";
+
+        }
+        [HttpDelete]
+        public char Delete()
+
+        {
+
+            Random random = new Random();
+            double flt = random.NextDouble();
+            int shift = Convert.ToInt32(Math.Floor(25 * flt));
+            char letter = Convert.ToChar(shift + 65);
+            return letter;
+
+        }
+        [HttpPatch]
+        public DateTime Patch()
+
+        {
+
+            DateTime localDate = DateTime.Now;
+
+            return localDate;
+
+        }
+        [HttpOptions]
+        public String Options()
+        {
+            return "GET: Return a Random Integer" + "POST: Return a Random Double" + "PUT: Return a String";
+        }
+    }
+}
---
+++ SimpleWebAPI/Models/Random.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebAPI.Models
+{
+    public class Random
+    {
+        public int shift { get; set; }
+        public double flt { get; set; }
+        public char letter { get; set; }
+        public DateTime date { get; set; }
+    }
+}
